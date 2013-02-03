b025effe8c651b07028bff584d61453614c70d1f
Marcus <m.brandenburger@tu-bs.de>
2013-02-03 20:58:15 +0100

    add some comments and refactor regex

--- src/main/java/protocol/common/ProtocolHandler.java
+++ src/main/java/protocol/common/ProtocolHandler.java
@@ -1,8 +1,10 @@
 package protocol.common;
 
-import org.apache.commons.lang3.StringUtils;
 import org.jboss.netty.channel.*;
 import org.slf4j.LoggerFactory;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.jboss.netty.channel.Channels.fireMessageReceived;
 
@@ -17,51 +19,86 @@ public class ProtocolHandler extends SimpleChannelHandler {
     private static final org.slf4j.Logger log =
             LoggerFactory.getLogger(ProtocolHandler.class);
 
-    //    static final String FIRST_LINE_REGEX = "DO\\s{1}[\\w/_-]*";
-    //    static final String HEADER_REGEX = "[\\w/_-]*:\\s{1}[\\w-]*";
-    public static final String FIRST_LINE_REGEX = "DO\\s[\\w/_-]*";
-    public static final String HEADER_REGEX = "[\\w/_-]*:\\s[\\w-]*";
+    private static final Pattern FIRST_LINE_REGEX =
+            Pattern.compile("DO\\s+([\\w/_-]+)");
+    private static final Pattern HEADER_REGEX =
+            Pattern.compile("([\\w-]+):\\s+([\\w/_-]+)");
 
     private ProtocolMessage bufferMessage;
 
     public void messageReceived(ChannelHandlerContext ctx, MessageEvent e)
             throws Exception {
 
+        // This is our protocol message decoder. It tries to decode each
+        // received line and put it into a ProtocolMessage object.
+        //
+        // If we have no bufferMessage, we would assume that this
+        // incoming message is the first line of a protocol message. Otherwise,
+        // this message is treated as a header.
+        //
+        // Each line will be verified with a appropriated regular expression
+        // pattern. In case of a mismatch, a BadRequest message will be sent and
+        // we close the connection.
+        //
+        // If we have a bufferMessage and receive a empty line, the
+        // bufferMessage will be forwarded to the next layer, in our case the
+        // ProtocolServerHandler
+
+        // Do not process any available message if channel was already closed.
+        if (!ctx.getChannel().isConnected()) {
+            return;
+        }
+
         if (this.bufferMessage == null) {
-            // read initial
-            String firstLine = (String) e.getMessage();
-            if (firstLine.matches(FIRST_LINE_REGEX)) {
-                createBufferMessage(firstLine);
+            Matcher matcher = FIRST_LINE_REGEX.matcher((String) e.getMessage());
+            if (matcher.matches()) {
+                this.bufferMessage =
+                        ProtocolMessageFactory.createRequest(matcher.group(1));
             } else {
-                protocolError(ctx, e, "InvalidFormatException");
+                // first line does not match expression
+                protocolError(ctx, "Invalid protocol exception");
             }
         } else {
             // read header line
             String headerLine = (String) e.getMessage();
-            // end of bufferMessage
+
             if (headerLine.isEmpty()) {
+                // end of bufferMessage
                 fireMessageReceived(ctx, resetBufferMessage());
             } else {
-                if (headerLine.matches(HEADER_REGEX)) {
-                    addHeader(headerLine);
+                Matcher matcher = HEADER_REGEX.matcher(headerLine);
+                if (matcher.matches()) {
+                    this.bufferMessage
+                            .addHeader(matcher.group(1), matcher.group(2));
                 } else {
-                    protocolError(ctx, e, "InvalidFormatException");
+                    // line does not match expression
+                    protocolError(ctx, "Invalid header format exception");
                 }
             }
         }
     }
 
+    @Override
     public void writeRequested(ChannelHandlerContext ctx, MessageEvent e)
             throws Exception {
 
+        // This is our protocol message encoder. It only forward
+        // ProtocolMessages to the next Layer. In our case it is a
+        // StringEncoder.
+
+        // do not process messages if channel was closed
+        if (!ctx.getChannel().isConnected()) {
+            return;
+        }
+
         if (e.getMessage() instanceof ProtocolMessage) {
             ProtocolMessage message = (ProtocolMessage) e.getMessage();
-            log.debug("Send " + message + " to " +
+            log.debug("Send " + message.getParameter() + " to " +
                     ctx.getChannel().getRemoteAddress());
 
             DownstreamMessageEvent dme =
                     new DownstreamMessageEvent(e.getChannel(), e.getFuture(),
-                            message.toStringMessage(), e.getRemoteAddress());
+                            message.serialize(), e.getRemoteAddress());
 
             ctx.sendDownstream(dme);
         }
@@ -73,31 +110,15 @@ public class ProtocolHandler extends SimpleChannelHandler {
         return message;
     }
 
-    private ProtocolMessage createBufferMessage(String parameter) {
-        this.bufferMessage = ProtocolMessageFactory
-                .createRequest(StringUtils.substring(parameter, 3));
-        return this.bufferMessage;
-    }
+    private void protocolError(ChannelHandlerContext ctx, String parameter) {
 
-    private void addHeader(String headerLine) {
-        // TODO validate headerLine before split and add
-        String[] keyValue = headerLine.split(": ");
-        this.bufferMessage.addHeader(keyValue[0], keyValue[1]);
-    }
-
-    private void protocolError(ChannelHandlerContext ctx, MessageEvent e,
-                               String parameter) {
         ProtocolMessage errorMessage =
                 ProtocolMessageFactory.createBadProtocolMessage(parameter);
 
         ChannelFuture channelFuture = ctx.getChannel().write(errorMessage);
 
-        channelFuture.addListener(new ChannelFutureListener() {
-            public void operationComplete(ChannelFuture future) {
-                Channel channel = future.getChannel();
-                channel.close();
-            }
-        });
+        channelFuture.addListener(ChannelFutureListener.CLOSE);
+        channelFuture.addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
     }
 
 }
