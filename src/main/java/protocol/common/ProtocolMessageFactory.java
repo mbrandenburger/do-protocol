b343f2e4a484a3a879413934ca7b4c9cae6fc139
Marcus <m.brandenburger@tu-bs.de>
2013-02-03 21:25:18 +0100

    Add java doc

--- src/main/java/protocol/common/ProtocolMessageFactory.java
+++ src/main/java/protocol/common/ProtocolMessageFactory.java
@@ -8,19 +8,37 @@ package protocol.common;
  */
 public class ProtocolMessageFactory {
 
-    // TODO create additional factory methods
-    // (e.g. ProtocolMessage with header)
+    // Not a real factory pattern yet, but could be helpful if protocol
+    // message complexity increases
+    //
+    // additional factory methods might be help (e.g. ProtocolMessage with
+    // header)
 
+    /**
+     * Create a protocol message with type BAD_REQUEST
+     * @param parameter the message parameter
+     * @return          a ProtocolMessage with
+     */
     public static ProtocolMessage createBadProtocolMessage(String parameter) {
         return new ProtocolMessage(ProtocolMessage.MessageType.BAD_REQUEST,
                 parameter);
     }
 
+    /**
+     * Create a protocol message with type RESPONSE
+     * @param parameter
+     * @return
+     */
     public static ProtocolMessage createResponse(String parameter) {
         return new ProtocolMessage(ProtocolMessage.MessageType.RESPONSE,
                 parameter);
     }
 
+    /**
+     * Create a protocol message with type REQUEST
+     * @param parameter
+     * @return
+     */
     public static ProtocolMessage createRequest(String parameter) {
         return new ProtocolMessage(ProtocolMessage.MessageType.REQUEST,
                 parameter);
